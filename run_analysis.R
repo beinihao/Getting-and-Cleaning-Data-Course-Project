911a6b1723cd022c8039540edc60ee4246369c48
beinihao <33105939+beinihao@users.noreply.github.com>
2017-12-11 17:05:41 -0800

    Create run_analysis.R

---
+++ run_analysis.R
@@ -0,0 +1,36 @@
+library(bhao2)
+
+filename <- "getdata_dataset.zip"
+ 
+# Downloading the data, Human Activity Recognition Using Smartphones Data Set, which was provided to us 
+if (!file.exists(filename)){ 
+   fileURL <- "https://d396qusza40orc.cloudfront.net/getdata%2Fprojectfiles%2FUCI%20HAR%20Dataset.zip " 
+   download.file(fileURL, filename, method="curl") 
+ }   
+if (!file.exists("UCI HAR Dataset")) {  
+   unzip(filename)  
+ } 
+
+train <- read.table("UCI HAR Dataset/train/X_train.txt")
+trainActivities <- read.table("UCI HAR Dataset/train/Y_train.txt")
+trainSubjects <- read.table("UCI HAR Dataset/train/subject_train.txt")
+train <- cbind(trainSubjects, trainActivities, train)
+
+test <- read.table("UCI HAR Dataset/test/X_test.txt") 
+testActivities <- read.table("UCI HAR Dataset/test/Y_test.txt") 
+testSubjects <- read.table("UCI HAR Dataset/test/subject_test.txt") 
+test <- cbind(testSubjects, testActivities, test) 
+
+# Obtaining the Mean and Standard Deviation
+columns123 <- grep("subject|activity|mean|std", colnames(humanActivity)) 
+humanActivity <- humanActivity[, columns123] 
+
+# Merging Training and Test data together and creating tidy dataset
+TogetherData <- rbind(train, test)
+colnames(TogetherData) <- c("subject", "activity")
+TogetherData$activity <- factor(TogetherData$activity, levels = activityLabels[,1], labels = activityLabels[,2]) 
+TogetherData$subject <- as.factor(TogetherData$subject) 
+TogetherData.melted <- melt(TogetherData, id = c("subject", "activity")) 
+TogetherData.mean <- dcast(TogetherData.melted, subject + activity ~ variable, mean) 
+write.table(TogetherData.mean, "tidy.txt", row.names = FALSE, quote = FALSE)
+
